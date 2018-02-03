e7e44131fba55ec2c3003c807f1f8c1317a8455d
Eric Berquist <eric.berquist@gmail.com>
2018-02-03 14:13:41 -0500

    binary tree implemented as array

--- ch4_array.py
+++ ch4_array.py
@@ -76,6 +76,65 @@ class MinHeap(Container):
         return
 
 
+class BinaryTree(Container):
+
+    def __init__(self, data=None):
+        super().__init__(data)
+
+    def size(self):
+        return len(self)
+
+    def min(self, i=0):
+        """Brute-force find the smallest element in the tree. Works if the
+        tree is not a binary search tree.
+        """
+        if not self._repr:
+            raise Exception
+        acc = self._repr[i]
+        left = 2*i + 1
+        right = 2*i + 2
+        if left < len(self):
+            acc = min(acc, self.min(left))
+        if right < len(self):
+            acc = min(acc, self.min(right))
+        return acc
+
+    def max(self, i=0):
+        """Brute-force find the largest element in the tree. Works if the tree
+        is not a binary search tree.
+        """
+        if not self._repr:
+            raise Exception
+        acc = self._repr[i]
+        left = 2*i + 1
+        right = 2*i + 2
+        if left < len(self):
+            acc = max(acc, self.max(left))
+        if right < len(self):
+            acc = max(acc, self.max(right))
+        return acc
+
+    def insert_complete(self, element):
+        self._repr.append(element)
+        return
+
+    def is_binary_search_tree(self, i=0):
+        # base case: going past the length of the array means we
+        # haven't failed, so must be a BST
+        if i >= self.size():
+            return True
+        left = 2*i + 1
+        right = 2*i + 2
+        has_left = left < len(self)
+        has_right = right < len(self)
+        if has_left and (not self.max(left) <= self._repr[i]):
+            return False
+        if has_right and (not self._repr[i] < self.min(right)):
+            return False
+        return self.is_binary_search_tree(left) \
+            and self.is_binary_search_tree(right)
+
+
 def test_minheap_extract():
     data_start = [2, 50, 23, 88, 90, 32, 74, 80]
     data_ref = [23, 50, 32, 88, 90, 80, 74]
@@ -99,6 +158,68 @@ def test_minheap_insert():
     return True
 
 
+complete_1 = BinaryTree([4])
+complete_2 = BinaryTree([4, 2])
+complete_3 = BinaryTree([10, 20, 30])
+complete_4 = BinaryTree([10, 20, 30, 40])
+is_bst = BinaryTree([8, 4, 10, 2, 6, 9])
+is_not_bst = BinaryTree([8, 4, 10, 2, 12, 9])
+bst_small_1 = BinaryTree([4, 2])
+bst_small_2 = BinaryTree([4, 2, 6])
+
+
+def test_binary_tree_size():
+    tests = [
+        (complete_1, 1),
+        (complete_2, 2),
+        (complete_3, 3),
+        (complete_4, 4),
+        (is_bst, 6),
+        (is_not_bst, 6),
+        (bst_small_1, 2),
+        (bst_small_2, 3),
+    ]
+    for (tree, outcome) in tests:
+        assert tree.size() == outcome
+    return True
+
+
+def test_binary_tree_min():
+    tests = [
+        (is_bst, 2),
+        (is_not_bst, 2),
+        (bst_small_1, 2),
+        (bst_small_2, 2),
+    ]
+    for (tree, outcome) in tests:
+        assert tree.min() == outcome
+    return True
+
+
+def test_binary_tree_max():
+    tests = [
+        (is_bst, 10),
+        (is_not_bst, 12),
+        (bst_small_1, 4),
+        (bst_small_2, 6),
+    ]
+    for (tree, outcome) in tests:
+        assert tree.max() == outcome
+    return True
+
+
+def test_is_binary_search_tree():
+    assert bst_small_1.is_binary_search_tree()
+    assert bst_small_2.is_binary_search_tree()
+    assert is_bst.is_binary_search_tree()
+    assert not is_not_bst.is_binary_search_tree()
+    return True
+
+
 if __name__ == '__main__':
     test_minheap_extract()
     test_minheap_insert()
+    test_binary_tree_size()
+    test_binary_tree_min()
+    test_binary_tree_max()
+    test_is_binary_search_tree()
