5327370db577873be9ba820b5f28b14871c23df4
Eric Berquist <eric.berquist@gmail.com>
2018-02-21 10:08:10 -0500

    ch10: bubble sort

--- ch10.py
+++ ch10.py
@@ -1,3 +1,38 @@
+def bubble_sort(l):
+    ln = len(l)
+    if ln == 1:
+        return
+    is_sorted = False
+    while not is_sorted:
+        # bubble pass
+        for i in range(1, ln):
+            if l[i - 1] > l[i]:
+                tmp = l[i - 1]
+                l[i - 1] = l[i]
+                l[i] = tmp
+        # check pass
+        pass_is_sorted = True
+        for i in range(1, ln):
+            if l[i - 1] > l[i]:
+                pass_is_sorted = False
+                break
+        if pass_is_sorted:
+            is_sorted = True
+    return
+
+
+def test_bubble_sort():
+    l1 = [9, 8, 7, 6, 5, -1, -2]
+    l1_ref = [-2, -1, 5, 6, 7, 8, 9]
+    bubble_sort(l1)
+    assert l1 == l1_ref
+    l2 = [2, 3, 4, 10, 20, 90]
+    l2_ref = l2.copy()
+    bubble_sort(l2)
+    assert l2 == l2_ref
+    return True
+
+
 def sorted_matrix_search(mat, element):
     """return the matrix tuple (i, j) of the element's location in the
     matrix; if not found, return None
@@ -11,12 +46,14 @@ def sorted_matrix_search(mat, element):
         return None
     if mat[m - 1][n - 1] < element:
         return None
+    # this is really searching for the midpoint
     for i in range(m):
         if mat[i][0] == element:
             return (i, 0)
         elif mat[i][0] > element:
             i = i - 1
             break
+    # this should be binary search
     for j in range(n):
         if mat[i][j] == element:
             return (i, j)
@@ -47,4 +84,5 @@ def test_sorted_matrix_search():
 
 
 if __name__ == '__main__':
+    test_bubble_sort()
     test_sorted_matrix_search()
