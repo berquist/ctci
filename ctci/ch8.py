a25619ab13e33d63c1eaca46a8f3eaa0e5047f9a
Eric Berquist <eric.berquist@gmail.com>
2018-04-02 10:55:03 -0400

    ctci ch8: sorted magic index search (working)

--- ctci/ch8.py
+++ ctci/ch8.py
@@ -2,24 +2,24 @@ def magic_index_sorted(arr, bot=None, top=None):
     if bot is None:
         bot = 0
     if top is None:
-        top = len(arr)
+        top = len(arr) - 1
+    if bot > top:
+        return None
     mid = (top + bot) // 2
-    ret = -1
     if arr[mid] == mid:
-        ret = mid
-    else:
-        ret_left = magic_index_sorted(arr, bot, mid - 1)
-        ret_right = magic_index_sorted(arr, mid + 1, top)
-        if ret_left != -1:
-            return ret_left
-        if ret_right != -1:
-            return ret_right
-    return ret
+        return mid
+    ret_left = magic_index_sorted(arr, bot, mid - 1)
+    ret_right = magic_index_sorted(arr, mid + 1, top)
+    if ret_left is not None:
+        return ret_left
+    if ret_right is not None:
+        return ret_right
 
 
 def test_magic_index_sorted():
     tests = [
         ([1, 2, 3, 3], 3),
+        ([1, 2, 3, 4, 5, 6, 10, 20, 8, 9], 8),
     ]
     for arr, outcome in tests:
         assert magic_index_sorted(arr) == outcome
