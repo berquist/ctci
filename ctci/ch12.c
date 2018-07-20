42d4cf6c34e6728c39c6b3bb2cab6280ace9257c
Eric Berquist <eric.berquist@gmail.com>
2018-07-20 13:50:02 -0400

    ctci ch12: problem 1 in C

--- ctci/ch12.c
+++ ctci/ch12.c
@@ -1,6 +1,50 @@
+#include <assert.h>
 #include <stddef.h> /* size_t */
-#include <stdlib.h> /* malloc, free */
+#include <stdlib.h> /* malloc/calloc, free */
 #include <stdio.h> /* printf */
+
+/* Write a method to print the last K lines of an input file using
+ * C.
+ */
+int ch12_1(size_t k) {
+    int ret;
+    const char filename[] = "CMakeCache.txt";
+    /* printf("filename: %s\n", filename); */
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+        perror("Error opening file");
+    size_t counter = 0;
+    char buf[BUFSIZ];
+    /* Count the number of lines in the file. */
+    while (fgets(buf, BUFSIZ, fp) != NULL) {
+        counter++;
+    }
+    rewind(fp);
+    assert(k < counter);
+    const size_t diff = counter - k;
+    for (size_t i = 0; i < diff; i++) {
+        fgets(buf, BUFSIZ, fp);
+    }
+    while (fgets(buf, BUFSIZ, fp) != NULL) {
+        ret = puts(buf);
+    }
+    ret = fclose(fp);
+    return ret;
+}
+
+/* Write an aligned malloc and free function that supports allocating
+ * memory such that the memory address returned is divisible by a
+ * specific power of two.
+ *
+ * EXAMPLE
+ *
+ * =align_malloc(1000, 128)= will return a memory address that is a
+ * multiple of 128 and that points to a memory of size 1000 bytes.
+ *
+ * =aligned_free()= will free the memory allocated by =align_malloc=.
+ */
+void ch12_10() {
+}
 
 /* Write a function C called =my2DAlloc= which allocates a
  * two-dimentional array. Minimize the number of calls to malloc and
@@ -25,14 +69,17 @@ void ch12_11(size_t nr, size_t nc) {
         for (size_t i = 0; i < nr; i++) {
             for (size_t j = 0; j < nc; j++) {
                 size_t idx = (i * nc) + j;
-                printf("arr[%d][%d] = %lf\n", i, j, arr[i][j]);
+                /* printf("arr[%d][%d] = %lf\n", i, j, arr[i][j]); */
             }
         }
         free(arr);
     }
 }
 
-int main() {
+int main(int argc, char **argv) {
+    const size_t k = 20;
+    ch12_1(k);
+    ch12_10();
     const size_t nr = 5;
     const size_t nc = 3;
     ch12_11(nr, nc);
